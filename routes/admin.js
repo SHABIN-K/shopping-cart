68ef8ec0d7f13c7058c2569c846b93cb908a6897
Shabin-k <helloshabink@gmail.com>
2023-02-25 20:37:23 +0530

    added add-products and view-produts with admin access

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET Admin listing. */
 router.get('/', function(req, res, next) {
-  res.render('index')
+  let products = [{
+    name: 'iphone 11',
+    category:'Mobile',
+    description:'The iPhone 13 has a 6.1-inch (15 cm) IPS LCD with a resolution is 1792 × 828 pixels',
+    imgURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi19me2QrJE5Q7-Pg-xv2gaYciIgQdm3mmnQ&usqp=CAU'
+  },{
+    name: 'iphone 12',
+    category:'Mobile',
+    description: 'The iPhone 13 has a 6.1-inch (15 cm) IPS LCD with a resolution is 1792 × 828 pixels',
+    imgURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYPxYoLD44K_DXjhxWFGEflEReeSFjq_rpnw&usqp=CAU'
+  },{
+    name: 'iphone 13',
+    category:'Mobile',
+    description: 'The iPhone 13 has a 6.1-inch (15 cm) IPS LCD with a resolution is 1792 × 828 pixels',
+    imgURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFk_WTMf3kw2fE1lL2IOgM_aMtGzEKTt2beg&usqp=CAU'
+  },{
+    name: 'iphone 14',
+    category:'Mobile',
+    description:'The iPhone 13 has a 6.1-inch (15 cm) IPS LCD with a resolution is 1792 × 828 pixels',
+    imgURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSL-LteDa-EtlptRoNCByPz2lDDl_x3dyuXw&usqp=CAU'
+  }]
+  res.render('admin/view-products', {products,admin :true})
+});
+
+router.get('/add-products', function(req,res){
+  res.render('admin/add-products', {admin :true})
+});
+
+router.post('/add-products', function(req,res){
+  console.log(req.body);
+  console.log(req.files.image);
+  //res.render('admin/add-products', {admin :true})
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -6,25 +6,26 @@ router.get('/', function(req, res, next) {
   let products = [{
     name: 'iphone 11',
     category:'Mobile',
-    description:'The iPhone 11 has a 6.1-inch (15 cm) IPS LCD with a resolution is 1792 × 828 pixels (1.4 megapixels) at a pixel density of 326 PPI with a maximum',
+    description:'The iPhone 13 has a 6.1-inch (15 cm) IPS LCD with a resolution is 1792 × 828 pixels',
     imgURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi19me2QrJE5Q7-Pg-xv2gaYciIgQdm3mmnQ&usqp=CAU'
   },{
     name: 'iphone 12',
     category:'Mobile',
-    description:'The iPhone 12 has a 6.1-inch (15 cm) IPS LCD with a resolution is 1792 × 828 pixels (1.4 megapixels) at a pixel density of 326 PPI with a maximum',
+    description:'The iPhone 13 has a 6.1-inch (15 cm) IPS LCD with a resolution is 1792 × 828 pixels',
     imgURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYPxYoLD44K_DXjhxWFGEflEReeSFjq_rpnw&usqp=CAU'
   },{
     name: 'iphone 13',
     category:'Mobile',
-    description:'The iPhone 13 has a 6.1-inch (15 cm) IPS LCD with a resolution is 1792 × 828 pixels (1.4 megapixels) at a pixel density of 326 PPI with a maximum',
+    description:'The iPhone 13 has a 6.1-inch (15 cm) IPS LCD with a resolution is 1792 × 828 pixels',
     imgURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFk_WTMf3kw2fE1lL2IOgM_aMtGzEKTt2beg&usqp=CAU'
   },{
     name: 'iphone 14',
     category:'Mobile',
-    description:'The iPhone 14 has a 6.1-inch (15 cm) IPS LCD with a resolution is 1792 × 828 pixels (1.4 megapixels) at a pixel density of 326 PPI with a maximum',
+    description:'The iPhone 14 has aixel density  a maximum',
     imgURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSL-LteDa-EtlptRoNCByPz2lDDl_x3dyuXw&usqp=CAU'
   }]
-  res.render('index', { products, admin:false });
+  res.render('index', { products, admin:false })
+
 });
 
 module.exports = router;
