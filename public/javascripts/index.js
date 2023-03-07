fdc7e9fec8f0c990ba99a9c4277cb5ac762afcfd
Shabin-k <helloshabink@gmail.com>
2023-03-08 00:07:27 +0530

    .

--- public/javascripts/index.js
+++
@@ -1,6 +0,0 @@
-    $(document).ready( function () {
-    $('#product-table').DataTable();
-    } );  
-    function viewImage(event){
-      document.getElementById('imgView').src=URL.createObjectURL(event.target.files[0])
-    }
---
+++ public/javascripts/script.js
@@ -0,0 +1,21 @@
+$(document).ready( function () {
+  $('#product-table').DataTable();
+});  
+
+function viewImage(event){
+  document.getElementById('imgView').src=URL.createObjectURL(event.target.files[0])
+}
+
+function addToCart(proID){
+  $.ajax({
+    url:'/add-to-cart/'+proID,
+    method:'get',
+    success:(response) =>{
+      if(response.status){
+        let count= $('#cart-count').html()
+        count=parseInt(count)+1
+        $("#cart-count").html(count)
+      }
+    }
+  })
+} 
